46ec1e2c165dd9480f0bfb085b2ed2fbbf84c8c7
OmarMalass <Omar1Malass@gmail.com>
2021-01-17 12:48:45 +0200

    Updated data.sql: - Initialized SALE and TRANSACTION tables.

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -16,3 +16,19 @@ INSERT INTO CLIENT(name, last_name, mobile_number) VALUES
 INSERT INTO SALESPERSON(name, last_name) VALUES
 ('Jim', 'Halpert'),
 ('Dwight', 'Schrute');
+
+
+INSERT INTO SALE(salesperson_id, client_id) VALUES
+(1,1),
+(1,2),
+(2,1),
+(2,2);
+
+INSERT INTO TRANSACTION(sale_id, product_id, quantity, unit_price) VALUES
+(1, 1, 50, 20),
+(1, 2, 2, 3000),
+(1, 4, 10, 40),
+(2, 3, 95, 1),
+(2, 1, 10, 1000),
+(3, 5, 100, 80),
+(4, 1, 17, 40);
